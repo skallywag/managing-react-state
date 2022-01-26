4f46a07bd8f8b23e3c91209be970323b242df32f
Samuel Lee <lee.sam1492@gmail.com>
2022-01-26 14:53:34 -0700

    learning react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,30 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-// import {getProducts} from './services/productService'
-import Spinner from "./Spinner";
-import UseFetch from "./services/useFetch";
-
+import Products from "./Products";
+import {Routes, Route} from 'react-router-dom'
+import Detail from './Detail'
+import Cart from "./Cart";
 
 
 
 export default function App() {
-  const [size, setSize] = useState('')
-
-  const { data: products, loading, error} = UseFetch("products?category=shoes")
-
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div> 
-    );
-  }
  
-  const filteredProducts = size ? products.filter((product) => product.skus.find((sku) => sku.size === parseInt(size))) : products
-
-  if(error) throw error
-  if (loading) return <Spinner /> 
-
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size" value={size} onChange={(e) => setSize(e.target.value)}>
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-          </section>
-          {size && <h2>Found {filteredProducts.length} items</h2>}
-          <section id="products">
-              {filteredProducts.map(renderProduct)}
-          </section>
+          <Routes>
+            <Route path="/" element={<h1>Weclome to carved rock fitness!</h1>}/>
+            <Route path="/:category" element={<Products />} />
+            <Route path="/:category/:id" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
         </main>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
